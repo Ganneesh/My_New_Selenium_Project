18224711d64ffa9b3e87b25ba4c9660397320ac5
Ganesh_Testing <gsmadane7@gmail.com>
2024-01-05 23:00:11 +0530

    WindowSize file

---
+++ src/test/java/browserconnectivity/WindowSize.java
@@ -0,0 +1,32 @@
+package browserconnectivity;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WindowSize {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+	
+		driver.get("https://google.com");
+
+		
+		Dimension size=driver.manage().window().getSize();
+		
+		System.out.println("Size of the window:- "+size.getHeight()+"   "+size.getWidth());
+		driver.quit();	
+		
+		
+		
+		
+	}
+
+}
